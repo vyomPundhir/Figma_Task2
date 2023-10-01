295b087f912bc18d83b2c259b807b3ed78f5581c
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-10-01 23:35:39 +0530

    Right.tsx file modified.

--- src/components/Right.tsx
+++ src/components/Right.tsx
@@ -1,11 +1,12 @@
 import Candidates from './Candidates'
 import Internship from './Internship'
+import "../index.css"
 
 const Right = () => {
   return (
-    <div className='pl-[100px] py-8 flex flex-row justify-between'>
-      <Internship/>
-      <Candidates/>
+    <div className='bg-[#e7e8ed] flex flex-row'>
+      <Internship />
+      <Candidates />
     </div>
   )
 }
