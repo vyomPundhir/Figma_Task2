103680aae6526d1adda9aae248c41ab295176588
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-10-03 23:35:14 +0530

    Candidates modified.

--- src/components/Candidates.tsx
+++ src/components/Candidates.tsx
@@ -18,7 +18,7 @@ const Candidates = () => {
       <div className="topbar flex flex-row justify-between mt-[20px]">
         <div className="topleft">
 
-          <select value={selectedCategory} onChange={handleCategoryChange} className="select w-[300px] text-[#1D4ED8] text-[17spx] font-[700]">
+          <select value={selectedCategory} onChange={handleCategoryChange} className="select w-[300px] text-[#1D4ED8] text-[17spx] font-[700] bg-[white]">
             <option disabled selected className="w-[300px] h-[40px]">Select a Category</option>
             <option className="text-[black]">All</option>
             <option className="text-[black]">Applied</option>
@@ -36,27 +36,27 @@ const Candidates = () => {
 
         <div className="topright flex flex-row items-center w-[380px] gap-[10px]">
           <div className="bg-[white] flex flex-row justify-center items-center w-[29px] h-[29px] rounded-[5px] cursor-pointer">
-            <img src="src\assets\tag.svg" alt="" className="w-[18px] h-[18px] bg-[white]"/>
+            <img src="src\assets\tag.png" alt="" className="w-[18px] h-[18px] bg-[white]"/>
           </div>
 
           <div className="bg-[white] flex flex-row justify-center items-center w-[28px] h-[28px] rounded-[5px] cursor-pointer">
-            <img src="src\assets\user-cross.svg" alt="" className="w-[22px] h-[22px] bg-white"/>
+            <img src="src\assets\user-cross.png" alt="" className="w-[22px] h-[22px] bg-white"/>
           </div>
 
           <div className="bg-[white] flex flex-row justify-center items-center w-[28px] h-[28px] rounded-[5px] cursor-pointer">
-            <img src="src\assets\user-check.svg" alt="" className="w-[22px] h-[22px] bg-white"/>
+            <img src="src\assets\user-check.png" alt="" className="w-[22px] h-[22px] bg-white"/>
           </div>
 
           <div className="bg-[white] flex flex-row justify-center items-center w-[28px] h-[28px] rounded-[5px] cursor-pointer">
-            <img src="src\assets\user-speak.svg" alt="" className="w-[22px] h-[22px] bg-white"/>
+            <img src="src\assets\user-speak.png" alt="" className="w-[22px] h-[22px] bg-white"/>
           </div>
 
           <div className="bg-[white] flex flex-row justify-center items-center w-[28px] h-[28px] rounded-[5px] cursor-pointer">
-            <img src="src\assets\mail.svg" alt="" className="w-[22px] h-[22px] bg-white"/>
+            <img src="src\assets\mail.png" alt="" className="w-[22px] h-[22px] bg-white"/>
           </div>
 
           <select className="px-2 text-[13px] text-[white] flex flex-row items-center bg-[#1D4ED8] h-[35px] rounded-[7px] gap-[5px] cursor-pointer">
-            <img src="src\assets\down-arrow-white.svg" alt="" className="w-[15px] h-[15px] bg-[#1D4ED8]"/>
+            <img src="src\assets\down-arrow-white.png" alt="" className="w-[15px] h-[15px] bg-[#1D4ED8]"/>
             <option disabled selected className="w-[300px] h-[40px]">Move to Video Interview |</option>
             <option className="text-[black]">Video Interview I</option>
             <option className="text-[black]">Video Interview II</option>
@@ -69,7 +69,7 @@ const Candidates = () => {
 
         <div className="rounded-[10px] px-[10px] py-[15px] list-top bg-white flex flex-row justify-between">
 
-          <div className="bg-transparent flex flex-row gap-[25px] items-center">
+          <div className="bg-white flex flex-row gap-[25px] items-center ">
             <input type="checkbox" name="" id="" className="w-[17px] h-[17px] cursor-pointer" />
 
             <span className="bg-transparent text-[14px] font-[700] text-[#1D4ED8]">{filteredData.length} Candidates</span>
